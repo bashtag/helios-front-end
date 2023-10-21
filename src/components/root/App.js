4245f6b7033782a376c4d2a12d0ff7462d155a51
bashtag <btstsiy@hotmail.com>
2023-10-21 21:34:12 +0300

    imports and routes

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,9 +1,21 @@
+import { Container } from "reactstrap";
+import Navi from "../navi/Navi";
+import Dashboard from "./Dashboard";
+import NotFound from "../common/NotFound";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return (
-    <div>
-      <header>Merhaba</header>
-    </div>
-  );
+	return (
+		<div>
+			<Container>
+				<Navi />
+				<Routes>
+					<Route index element={<Dashboard />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
